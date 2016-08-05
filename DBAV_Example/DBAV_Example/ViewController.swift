e4aac9c26529ed51ea5233beaaeaaa9388076452
Rigoberto Saenz Imbacuan <rigoberto.saenz@payulatam.com>
2016-08-04 22:47:27 -0500

    - Base project

---
+++ DBAV_Example/DBAV_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBAV_Example
+//
+//  Created by Rigoberto Sáenz Imbacuán on 8/4/16.
+//  Copyright © 2016 Rigoberto Sáenz Imbacuán [https://www.linkedin.com/in/rsaenzi]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
