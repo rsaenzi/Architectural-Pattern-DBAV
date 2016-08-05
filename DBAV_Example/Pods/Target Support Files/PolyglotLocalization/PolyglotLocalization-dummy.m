a995fe27c11f0ac1a8f2a4490c9f1af618561aeb
Rigoberto Saenz Imbacuan <rigoberto.saenz@payulatam.com>
2016-08-04 22:55:09 -0500

    - Cocoapods support added - Some core pods installed

---
+++ DBAV_Example/Pods/Target Support Files/PolyglotLocalization/PolyglotLocalization-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PolyglotLocalization : NSObject
+@end
+@implementation PodsDummy_PolyglotLocalization
+@end
---
+++ DBAV_Example/Pods/Target Support Files/PolyglotLocalization/PolyglotLocalization-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double PolyglotLocalizationVersionNumber;
+FOUNDATION_EXPORT const unsigned char PolyglotLocalizationVersionString[];
+
